4375f4ae30583f87b2b285a29a453a55cd013303
Myah <myah@Zipcoders-MacBook-Pro-61.local>
2022-07-02 14:10:22 -0400

    NumberUtilities test cases pass

--- src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
@@ -3,34 +3,65 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class NumberUtilities {
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        String str = "";
+        for (int i =start; i<= stop; i++) {
+            if (!(i%2 == 0)) {
+                str = (str+i);
+//                System.out.print(str);
+            }
+        }
+        return str;
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        String str = "";
+        for (int i =start; i<stop; i++) {
+            if ((i%2 == 0)) {
+                str = (str+i);
+//                System.out.print(str);
+            }
+//            else {
+//                System.out.println(str + i);
+//            }
+//            System.out.println();
+        }
+        return str;
     }
 
 
     public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
+        String str = "";
+        for (int i = start; i<stop; i+=step) {
+            str = str + String.valueOf(i*i);
+        }
+//        System.out.print(str);
+        return str;
     }
 
-    public static String getRange(int start) {
-        return null;
+    public static String getRange(int stop) {
+        return NumberUtilities.getRange(0, stop, 1);
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+        return NumberUtilities.getRange(start,stop, 1);
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        String strB = "";
+        for (int i=start; i< stop; i += step) {
+            strB = strB + String.valueOf(i);
+        }
+        return strB;
     }
 
 
     public static String getExponentiations(int start, int stop, int step, int exponent) {
-        return null;
+        String str = "";
+        for (int i=start; i < stop; i+= step) {
+            str = str + String.valueOf((int)Math.pow(i, exponent));
+        }
+        return str;
     }
 }
