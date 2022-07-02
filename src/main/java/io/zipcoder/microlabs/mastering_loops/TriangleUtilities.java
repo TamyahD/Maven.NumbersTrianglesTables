b3e21b631ff2e724a7a696f0daa949c16d6f477c
Myah <myah@Zipcoders-MacBook-Pro-61.local>
2022-07-02 14:11:28 -0400

    TriangleUtilities test cases pass

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
@@ -3,18 +3,31 @@ package io.zipcoder.microlabs.mastering_loops;
 public class TriangleUtilities {
 
     public static String getTriangle(int numberOfRows) {
-        return null;
+        StringBuilder sb = new StringBuilder();
+
+        for (int idx = 1; idx < numberOfRows; idx++) {
+            sb.append(TriangleUtilities.getRow(idx));
+            sb.append('\n');
+        }
+//        System.out.print(sb.toString());
+        return sb.toString();
     }
 
     public static String getRow(int numberOfStars) {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        for (int idx = 0; idx < numberOfStars; idx++) {
+            sb.append("*");
+        }
+        return sb.toString();
     }
 
     public static String getSmallTriangle() {
-        return null;
+//        System.out.print(getTriangle(5));
+        return getTriangle(5);
     }
 
     public static String getLargeTriangle() {
-        return null;
+//        System.out.print(getTriangle(10));
+        return getTriangle(10);
     }
 }
